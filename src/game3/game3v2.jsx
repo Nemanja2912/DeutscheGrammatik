a3998506c88d04d7088ccb3655dc5391e9e7d314
Nemanja <nemanja.mijailovic2912@gmail.com>
2021-11-20 11:38:07 +0100

    4/5 done game3

--- src/game3/game3v2.jsx
+++ src/game3/game3v2.jsx
@@ -1,6 +1,8 @@
-import React, { createRef, useEffect } from "react";
+import React, { createRef, useState, useRef } from "react";
 import "../css/game3v2.css";
 import Linev2 from "./linev2";
+import Info from "../component/info";
+import Finger from "../assets/img/finger.svg";
 
 const lists = [
   ["Michaela", "repariert", "das Auto."],
@@ -8,23 +10,82 @@ const lists = [
   ["in einem Café.", "sitzen", "Sie", "am Abend"],
 ];
 
-console.log(lists[0][3]);
-
 const Game3v2 = () => {
+  const [lineShow, setLineShow] = useState(0);
+  const [indicator, setIndicator] = useState(false);
+  const [wrong, setWrong] = useState(false);
+  const [info, setInfo] = useState(true);
+  const [fingerPos, setFingerPos] = useState([window.innerWidth - 100, 100]);
+  const [fingerHide, setFingerHide] = useState(false);
+  const [fingerShow, setFingerShow] = useState(false);
+  const verbRef = useRef();
+  const helpRef = useRef();
+
+  const handleHelp = () => {
+    setFingerShow(true);
+
+    setFingerPos([
+      verbRef.current.offsetLeft + 5,
+      verbRef.current.offsetTop + 5,
+    ]);
+    setTimeout(() => {
+      helpRef.current.click();
+    }, 1000);
+
+    setTimeout(() => {
+      setFingerHide(true);
+      setFingerShow(false);
+      setFingerPos([window.innerWidth - 100, 100]);
+
+      setTimeout(() => {
+        setFingerHide(false);
+      }, 0);
+    }, 2000);
+  };
+
   return (
-    <div className="game3v2">
-      {lists.map((list, index) => (
-        <Linev2
-          leftWord={list[0]}
-          centerWord={list[1]}
-          rightWord1={list[2]}
-          rightWord2={list[3] === undefined ? false : list[3]}
-          list={list}
-          index={index + 1}
-          nounIndex={index === 2 ? 2 : 0}
+    <>
+      {info && (
+        <Info
+          desc="Ziehe das Verb aus jedem Satz auf den blauen Balken."
+          setInfo={setInfo}
         />
-      ))}
-    </div>
+      )}
+      <div className="help-bar">
+        <div className="info" onClick={() => setInfo(true)}>
+          i
+        </div>
+        <div className="help" onClick={handleHelp}>
+          ?
+        </div>
+      </div>
+      <div className="help-overlay" style={{ opacity: fingerShow ? 1 : 0 }}>
+        {!fingerHide && (
+          <img
+            src={Finger}
+            alt=""
+            style={{
+              left: fingerPos[0],
+              top: fingerPos[1],
+            }}
+            className={`${fingerShow ? "show" : ""}`}
+          />
+        )}
+      </div>
+      <div className="game3v2">
+        {lists.map((list, index) => (
+          <Linev2
+            leftWord={list[0]}
+            centerWord={list[1]}
+            rightWord1={list[2]}
+            rightWord2={list[3] === undefined ? false : list[3]}
+            list={list}
+            index={index + 1}
+            nounIndex={index === 2 ? 2 : 0}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 
--- src/game3/linev2.jsx
+++ src/game3/linev2.jsx
@@ -1,4 +1,4 @@
-import React, { createRef, useEffect } from "react";
+import React, { createRef, useEffect, useRef, useState } from "react";
 import "../css/game3v2.css";
 
 const Linev2 = ({
@@ -15,8 +15,10 @@ const Linev2 = ({
     reciArr[i] = createRef();
   }
 
-  const p = [];
-  const reciArrValues = [];
+  let centerRef = useRef();
+  let circleRef = useRef();
+
+  const [reciArrValues, setReciArrValues] = useState([]);
   const pos = [];
   let prihvatamoPos = true;
   let pomerajStari;
@@ -28,20 +30,124 @@ const Linev2 = ({
 
   useEffect(() => {
     initValues();
+
+    let leftPos = centerRef.current.getBoundingClientRect().left;
+
+    let centerPos = {
+      left: centerRef.current.getBoundingClientRect().left - 15,
+      right:
+        window.innerWidth -
+        centerRef.current.getBoundingClientRect().right -
+        15,
+      top: centerRef.current.getBoundingClientRect().top - 40,
+      bottom:
+        window.innerHeight -
+        centerRef.current.getBoundingClientRect().bottom -
+        40,
+    };
+
+    circleRef.current.style.left = centerPos.left + "px";
+    circleRef.current.style.right = centerPos.right + "px";
+    circleRef.current.style.top = centerPos.top + "px";
+    circleRef.current.style.bottom = centerPos.bottom + "px";
+
+    console.log(centerRef.current.getBoundingClientRect().top);
+    console.log(
+      window.innerHeight - centerRef.current.getBoundingClientRect().bottom
+    );
+
+    if (reciArr[nounIndex].current.getBoundingClientRect().left < leftPos) {
+      circleRef.current.style.left =
+        reciArr[nounIndex].current.getBoundingClientRect().left - 15 + "px";
+    } else {
+      circleRef.current.style.right =
+        window.innerWidth -
+        reciArr[nounIndex].current.getBoundingClientRect().right -
+        15 +
+        "px";
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleMove = (i) => {
+    const posXLeft = reciArr[i].current.getBoundingClientRect().left;
+    const posXRight = reciArr[i].current.getBoundingClientRect().right;
+    const posYTop = reciArr[i].current.getBoundingClientRect().top;
+    const posYBottom = reciArr[i].current.getBoundingClientRect().bottom;
+
     reciArr[i].current.classList.remove("transition");
+
+    if (i === nounIndex) {
+      circleRef.current.classList.remove("opacity");
+    }
     const initLeft = reciArrValues[i].leftRelative;
     let didMove = false;
     prihvatamoPos = false;
     const move = (e) => {
-      reciArr[i].current.style.zIndex = "100000000";
+      reciArr[i].current.style.zIndex = "10000000";
 
       if (i === 1) {
         reciArr[i].current.style.left = initLeft + e.clientX - pos[0] + "px";
         reciArr[i].current.style.top = e.clientY - pos[1] + "px";
         return;
+      }
+
+      if (i === nounIndex) {
+        if (
+          e.clientX - (pos[0] - posXLeft) <
+            centerRef.current.getBoundingClientRect().left -
+              reciArr[i].current.getBoundingClientRect().width -
+              50 ||
+          e.clientX - (pos[0] - posXRight) >
+            centerRef.current.getBoundingClientRect().right +
+              reciArr[i].current.getBoundingClientRect().width +
+              50 ||
+          e.clientY - (pos[1] - posYTop) <
+            centerRef.current.getBoundingClientRect().top - 50 ||
+          e.clientY - (pos[1] - posYBottom) >
+            centerRef.current.getBoundingClientRect().bottom + 50
+        ) {
+          moveEnd();
+          return;
+        }
+
+        if (
+          reciArr[i].current.getBoundingClientRect().left <
+          centerRef.current.getBoundingClientRect().left
+        ) {
+          circleRef.current.style.left =
+            reciArr[nounIndex].current.getBoundingClientRect().left - 15 + "px";
+        }
+
+        if (
+          reciArr[i].current.getBoundingClientRect().right >
+          centerRef.current.getBoundingClientRect().right
+        ) {
+          circleRef.current.style.right =
+            window.innerWidth -
+            reciArr[nounIndex].current.getBoundingClientRect().right -
+            15 +
+            "px";
+        }
+
+        if (
+          reciArr[i].current.getBoundingClientRect().top <
+          centerRef.current.getBoundingClientRect().top
+        ) {
+          circleRef.current.style.top =
+            reciArr[nounIndex].current.getBoundingClientRect().top - 40 + "px";
+        }
+
+        if (
+          reciArr[i].current.getBoundingClientRect().bottom >
+          centerRef.current.getBoundingClientRect().bottom
+        ) {
+          circleRef.current.style.bottom =
+            window.innerHeight -
+            reciArr[nounIndex].current.getBoundingClientRect().bottom -
+            40 +
+            "px";
+        }
       }
 
       reciArr[i].current.style.left = initLeft + e.clientX - pos[0] + "px";
@@ -56,7 +162,6 @@ const Linev2 = ({
         if (novi.dynamicID > 2) return;
       }
       if (novi.imenica) {
-        console.log(stari.dynamicID);
         if (stari.dynamicID > 2) return;
       }
       if (indexNovi === -1 || indexNovi === 1) return;
@@ -68,7 +173,7 @@ const Linev2 = ({
       } else {
         return;
       }
-      if (stari.dynamicID == novi.dynamicID) return;
+      if (stari.dynamicID === novi.dynamicID) return;
       const overCenter = novi.dynamicID < 1 || stari.dynamicID < 1;
       const saDesne = !overCenter;
       if (saDesne) {
@@ -122,7 +227,7 @@ const Linev2 = ({
           let pomerajNovi = 0;
           let rec;
 
-          if (noviStrana == "r") {
+          if (noviStrana === "r") {
             pomerajNovi += -novi.width - centarWidth;
             rec = reciArrValues.find((t) => t.dynamicID === 2);
             pomerajNovi -= reciArr.length > 3 ? rec.width : 0;
@@ -133,7 +238,7 @@ const Linev2 = ({
             pomerajNovi += reciArr.length > 3 ? rec.width : 0;
           }
           pomerajStari = 0;
-          if (noviStrana == "r") {
+          if (noviStrana === "r") {
             pomerajStari -= -stari.width - centarWidth;
 
             rec = reciArrValues.find((t) => t.dynamicID === 2);
@@ -152,18 +257,18 @@ const Linev2 = ({
           const noviStrana = novi.dynamicID > stari.dynamicID ? "r" : "l";
           const centarWidth = reciArrValues[1].width;
           const pomerajNovi =
-            noviStrana == "r"
+            noviStrana === "r"
               ? -novi.width - centarWidth
               : novi.width + centarWidth;
           pomerajStari =
-            noviStrana == "r"
+            noviStrana === "r"
               ? stari.width + centarWidth
               : -stari.width - centarWidth;
           reciArr[novi.id].current.style.left =
             reciArrValues[novi.id].leftRelative + pomerajNovi + "px";
 
           const sirinaRazlika =
-            noviStrana == "r"
+            noviStrana === "r"
               ? stari.width - novi.width
               : novi.width - stari.width;
 
@@ -185,9 +290,30 @@ const Linev2 = ({
       }
     };
     const moveEnd = () => {
+      circleRef.current.classList.add("opacity");
+
+      let centerPos = {
+        left: centerRef.current.getBoundingClientRect().left - 15,
+        right:
+          window.innerWidth -
+          centerRef.current.getBoundingClientRect().right -
+          15,
+        top: centerRef.current.getBoundingClientRect().top - 40,
+        bottom:
+          window.innerHeight -
+          centerRef.current.getBoundingClientRect().bottom -
+          40,
+      };
+
+      circleRef.current.style.left = centerPos.left + "px";
+      circleRef.current.style.right = centerPos.right + "px";
+      circleRef.current.style.top = centerPos.top + "px";
+      circleRef.current.style.bottom = centerPos.bottom + "px";
+
       if (i === 1) {
         reciArr[1].current.style.left = 0 + "px";
         reciArr[1].current.style.top = 0 + "px";
+
         document.removeEventListener("mousemove", move);
         document.removeEventListener("mouseup", moveEnd);
         return;
@@ -202,6 +328,24 @@ const Linev2 = ({
       reciArr[i].current.style.top = "0px";
       document.removeEventListener("mouseup", moveEnd);
       prihvatamoPos = true;
+
+      setTimeout(() => {
+        if (
+          reciArr[nounIndex].current.getBoundingClientRect().left <
+          centerPos.left + 15
+        ) {
+          circleRef.current.style.left =
+            reciArr[nounIndex].current.getBoundingClientRect().left - 15 + "px";
+        } else {
+          circleRef.current.style.right =
+            window.innerWidth -
+            reciArr[nounIndex].current.getBoundingClientRect().right -
+            15 +
+            "px";
+
+          console.log(circleRef.current.style.right);
+        }
+      }, 300);
     };
     document.addEventListener("mousemove", move);
     document.addEventListener("mouseup", moveEnd);
@@ -211,8 +355,8 @@ const Linev2 = ({
     for (let i = 0; i < reciArr.length; i++) {
       const id = i;
       let side;
-      if (i == 0) side = "left";
-      else if (i == 1) side = "center";
+      if (i === 0) side = "left";
+      else if (i === 1) side = "center";
       else side = "right";
       const boundingRect = reciArr[i].current.getBoundingClientRect();
       const left = boundingRect.left;
@@ -226,10 +370,11 @@ const Linev2 = ({
         width,
         leftRelative: parseFloat(reciArr[i].current.style.left),
         imenica: i === nounIndex,
-        allowListedIDS: i == 0 ? [2] : [],
+        allowListedIDS: i === 0 ? [2] : [],
         dynamicID: id,
       };
       reciArrValues[i] = obj;
+      setReciArrValues(reciArrValues);
     }
   }
   function calcValue(
@@ -241,8 +386,8 @@ const Linev2 = ({
   ) {
     let i = id;
     let side;
-    if (dynamicID == 0) side = "left";
-    if (dynamicID == 1) side = "center";
+    if (dynamicID === 0) side = "left";
+    if (dynamicID === 1) side = "center";
     else side = "right";
     const boundingRect = reciArr[i].current.getBoundingClientRect();
     const left = pomeraj ? reciArrValues[i].left + pomeraj : boundingRect.left;
@@ -272,10 +417,10 @@ const Linev2 = ({
   }
 
   return (
-    <div class={`container ${"line" + index}`}>
+    <div className={`container ${"line" + index}`}>
       <div id="levo">
         <div
-          class="word transition"
+          className="word transition"
           ref={reciArr[0]}
           style={{ left: 0, top: 0 }}
           onMouseMove={(e) => setPos(e)}
@@ -286,9 +431,9 @@ const Linev2 = ({
           </div>
         </div>
       </div>
-      <div id="centar">
+      <div id="centar" ref={centerRef}>
         <div
-          class="word transition"
+          className="word transition"
           ref={reciArr[1]}
           style={{ left: 0, top: 0 }}
           onMouseMove={(e) => setPos(e)}
@@ -301,7 +446,7 @@ const Linev2 = ({
       </div>
       <div id="desno">
         <div
-          class="word transition"
+          className="word transition"
           ref={reciArr[2]}
           style={{ left: 0, top: 0 }}
           onMouseMove={(e) => setPos(e)}
@@ -313,7 +458,7 @@ const Linev2 = ({
         </div>
         {rightWord2 && (
           <div
-            class="word transition"
+            className="word transition"
             ref={reciArr[3]}
             style={{ left: 0, top: 0 }}
             onMouseMove={(e) => setPos(e)}
@@ -325,6 +470,8 @@ const Linev2 = ({
           </div>
         )}
       </div>
+
+      {<div className="circle opacity" ref={circleRef}></div>}
     </div>
   );
 };
